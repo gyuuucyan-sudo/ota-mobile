c9f63f49ced5002e4ff9829e0e0f1acef0c74a86
gyuuucyan-sudo <gyuuucyan@gmail.com>
2026-06-14 17:14:17 +0800

    Add files via upload

---
+++ roomDatabase.js
@@ -0,0 +1,211 @@
+window.roomDatabase = {
+    "WAGAMAMA梅島101": {
+        displayName: "WAGAMAMA梅島101",
+        aliases: ["WAGAMAMA梅島101", "梅島101", "梅岛101", "umejima101", "Umejima101"],
+        area: "30.55㎡",
+        floor: "4階建マンションの1階",
+        bedding: "ダブルベッド1台、シングルベッド1台、掛布団2つ（ダブル1・シングル1）、枕3つ",
+        maxGuests: "3人",
+        wifi: {
+            id24: "SSID-aTFHtd",
+            id5: "SSID-aTFHtd",
+            password: "MfDGLdHA"
+        },
+        kitchen: "フォーク×3、スプーン×3、テーブルナイフ×3、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島102": {
+        displayName: "WAGAMAMA梅島102",
+        aliases: ["WAGAMAMA梅島102", "梅島102", "梅岛102", "umejima102", "Umejima102"],
+        area: "34.44㎡",
+        floor: "4階建マンションの1階",
+        bedding: "ダブルベッド2台、掛布団2つ、枕4つ",
+        maxGuests: "4人",
+        wifi: {
+            id24: "SSID-a7FdHF",
+            id5: "SSID-a7FdHF",
+            password: "JEYd4EHm"
+        },
+        kitchen: "フォーク×4、スプーン×4、テーブルナイフ×4、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島201": {
+        displayName: "WAGAMAMA梅島201",
+        aliases: ["WAGAMAMA梅島201", "梅島201", "梅岛201", "umejima201", "Umejima201"],
+        area: "30.55㎡",
+        floor: "4階建マンションの2階",
+        bedding: "ダブルベッド1台、シングルベッド1台、掛布団2つ（ダブル1・シングル1）、枕3つ",
+        maxGuests: "3人",
+        wifi: {
+            id24: "SSID-AeMdYG",
+            id5: "SSID-AeMdYG",
+            password: "tL4dFHe4"
+        },
+        kitchen: "フォーク×3、スプーン×3、テーブルナイフ×3、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島202": {
+        displayName: "WAGAMAMA梅島202",
+        aliases: ["WAGAMAMA梅島202", "梅島202", "梅岛202", "umejima202", "Umejima202"],
+        area: "33.44㎡",
+        floor: "4階建マンションの2階",
+        bedding: "ダブルベッド2台、掛布団2つ、枕4つ",
+        maxGuests: "4人",
+        wifi: {
+            id24: "SSID-TDFDRN",
+            id5: "SSID-TDFDRN",
+            password: "LdEnJALD"
+        },
+        kitchen: "フォーク×4、スプーン×4、テーブルナイフ×4、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島203": {
+        displayName: "WAGAMAMA梅島203",
+        aliases: ["WAGAMAMA梅島203", "梅島203", "梅岛203", "umejima203", "Umejima203"],
+        area: "23.10㎡",
+        floor: "4階建マンションの2階",
+        bedding: "シングルベッド2台、掛布団2つ、枕2つ",
+        maxGuests: "2人",
+        wifi: {
+            id24: "SSID-FteTha",
+            id5: "SSID-FteTha",
+            password: "hTRLMaRf"
+        },
+        kitchen: "フォーク×2、スプーン×2、テーブルナイフ×2、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島301": {
+        displayName: "WAGAMAMA梅島301",
+        aliases: ["WAGAMAMA梅島301", "梅島301", "梅岛301", "umejima301", "Umejima301"],
+        area: "30.55㎡",
+        floor: "4階建マンションの3階",
+        bedding: "ダブルベッド1台、シングルベッド1台、掛布団2つ（ダブル1・シングル1）、枕3つ",
+        maxGuests: "3人",
+        wifi: {
+            id24: "SSID-MtNadE",
+            id5: "SSID-MtNadE",
+            password: "hAHeaLHr"
+        },
+        kitchen: "フォーク×3、スプーン×3、テーブルナイフ×3、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島302": {
+        displayName: "WAGAMAMA梅島302",
+        aliases: ["WAGAMAMA梅島302", "梅島302", "梅岛302", "umejima302", "Umejima302"],
+        area: "33.44㎡",
+        floor: "4階建マンションの3階",
+        bedding: "ダブルベッド2台、掛布団2つ、枕4つ",
+        maxGuests: "4人",
+        wifi: {
+            id24: "SSID-GfeJ3M",
+            id5: "SSID-GfeJ3M",
+            password: "YMRf37rh"
+        },
+        kitchen: "フォーク×4、スプーン×4、テーブルナイフ×4、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島303": {
+        displayName: "WAGAMAMA梅島303",
+        aliases: ["WAGAMAMA梅島303", "梅島303", "梅岛303", "umejima303", "Umejima303"],
+        area: "23.10㎡",
+        floor: "4階建マンションの3階",
+        bedding: "シングルベッド2台、掛布団2つ、枕2つ",
+        maxGuests: "2人",
+        wifi: {
+            id24: "SSID-FmMrGJ",
+            id5: "SSID-FmMrGJ",
+            password: "AmFDdmAL"
+        },
+        kitchen: "フォーク×2、スプーン×2、テーブルナイフ×2、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島401": {
+        displayName: "WAGAMAMA梅島401",
+        aliases: ["WAGAMAMA梅島401", "梅島401", "梅岛401", "umejima401", "Umejima401"],
+        area: "30.55㎡",
+        floor: "4階建マンションの4階",
+        bedding: "ダブルベッド1台、シングルベッド1台、掛布団2つ（ダブル1・シングル1）、枕3つ",
+        maxGuests: "3人",
+        wifi: {
+            id24: "SSID-AJd3Ym",
+            id5: "SSID-AJd3Ym",
+            password: "3EMer7HH"
+        },
+        kitchen: "フォーク×3、スプーン×3、テーブルナイフ×3、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島402": {
+        displayName: "WAGAMAMA梅島402",
+        aliases: ["WAGAMAMA梅島402", "梅島402", "梅岛402", "umejima402", "Umejima402"],
+        area: "33.44㎡",
+        floor: "4階建マンションの4階",
+        bedding: "ダブルベッド2台、掛布団2つ、枕4つ",
+        maxGuests: "4人",
+        wifi: {
+            id24: "SSID-YLAtAm",
+            id5: "SSID-YLAtAm",
+            password: "AFE7fnFT"
+        },
+        kitchen: "フォーク×4、スプーン×4、テーブルナイフ×4、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA梅島403": {
+        displayName: "WAGAMAMA梅島403",
+        aliases: ["WAGAMAMA梅島403", "梅島403", "梅岛403", "umejima403", "Umejima403"],
+        area: "23.10㎡",
+        floor: "4階建マンションの4階",
+        bedding: "シングルベッド2台、掛布団2つ、枕2つ",
+        maxGuests: "2人",
+        wifi: {
+            id24: "SSID-Tdrret",
+            id5: "SSID-Tdrret",
+            password: "LHAGemJ7"
+        },
+        kitchen: "フォーク×2、スプーン×2、テーブルナイフ×2、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA柴又 松": {
+        displayName: "WAGAMAMA柴又 松",
+        aliases: ["WAGAMAMA柴又 松", "柴又 松", "柴又松", "shibamata matsu", "Shibamata Matsu"],
+        area: "86.93㎡",
+        floor: "2階建戸建",
+        bedding: "1階：シングル布団3つ、枕3つ。2階手前の部屋：シングルベッド2台、枕2つ。2階奥の部屋：シングルベッド1台、ダブルベッド1台、枕3つ",
+        maxGuests: "9人",
+        wifi: {
+            id24: "TP-Link_282A",
+            id5: "TP-Link_282A",
+            password: "13261924"
+        },
+        kitchen: "フォーク×9、スプーン×9、テーブルナイフ×9、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA柴又 竹101": {
+        displayName: "WAGAMAMA柴又 竹101",
+        aliases: ["WAGAMAMA柴又 竹101", "柴又 竹101", "柴又竹101", "shibamata take101", "Shibamata Take101"],
+        area: "86.68㎡",
+        floor: "2階建マンションの1階",
+        bedding: "ダブルベッド2台、枕4つ",
+        maxGuests: "4人",
+        wifi: {
+            id24: "TP-Link_282D",
+            id5: "TP-Link_282D",
+            password: "41098059"
+        },
+        kitchen: "フォーク×4、スプーン×4、テーブルナイフ×4、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    },
+
+    "WAGAMAMA柴又 竹201": {
+        displayName: "WAGAMAMA柴又 竹201",
+        aliases: ["WAGAMAMA柴又 竹201", "柴又 竹201", "柴又竹201", "shibamata take201", "Shibamata Take201"],
+        area: "86.68㎡",
+        floor: "2階建マンションの2階",
+        bedding: "ダブルベッド2台、枕4つ",
+        maxGuests: "4人",
+        wifi: {
+            id24: "TP-Link_282D",
+            id5: "TP-Link_282D",
+            password: "41098059"
+        },
+        kitchen: "フォーク×4、スプーン×4、テーブルナイフ×4、玉杓子×1、フライ返し×1、キッチンバサミ×1、包丁×1、まな板×1、フライパンセット×1"
+    }
+};
